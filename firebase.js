89d8dd09d62997c259c31271e081b3c2ef15ec75
Habteab Teame Kiros <habteabtk@uia.no>
2026-02-06 12:27:50 +0100

    Secure config: env vars and ignore secrets

--- firebase.js
+++ firebase.js
@@ -1,30 +1,9 @@
-import { initializeApp } from "firebase/app";
-import {
-  getAuth,
-  initializeAuth,
-  getReactNativePersistence,
-} from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
 const firebaseConfig = {
-  apiKey: "key",
-  authDomain: "auD",
-  projectId: "pId",
-  storageBucket: "Sb",
-  messagingSenderId: "mSId",
-  appId: "appId",
-  measurementId: "mid",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
-
-// Initialize Firebase App
-const app = initializeApp(firebaseConfig);
-
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(AsyncStorage),
-});
-
-// Get Firestore Instance
-const db = getFirestore(app);
-
-export { db, auth };
